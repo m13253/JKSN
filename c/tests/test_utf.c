71402cc7ec9ba1986a9493817013fd210a8b7ed1
Star Brilliant <m13253@hotmail.com>
2014-08-24 00:02:01 +0800

    Try to write a C implementation

--- c/tests/test_utf.c
+++ c/tests/test_utf.c
@@ -11,23 +11,23 @@ int main(int argc, char *argv[]) {
             break;
         }
     if(to_utf8) {
-        uint16_t buf[1024];
+        uint16_t buf[1024] = {0};
         if(fgets((char *) buf, 2048, stdin)) {
             size_t len;
             for(len = 0; buf[len]; len++) {
             }
-            fprintf(stderr, "Input length = %zu\n", len);
+            fprintf(stderr, "Input UTF-16 size: %zu\n", len);
             char *out = malloc(jksn_utf16_to_utf8(NULL, buf, len));
             fwrite(out, 1, jksn_utf16_to_utf8(out, buf, len), stdout);
             free(out);
         }
     } else {
-        char buf[1024];
-        if(fgets((char *) buf, 1024, stdin)) {
+        char buf[1024] = "0";
+        if(fgets(buf, 1024, stdin)) {
             size_t len;
             for(len = 0; buf[len]; len++) {
             }
-            fprintf(stderr, "Input length = %zu\n", len);
+            fprintf(stderr, "Input UTF-8 size: %zu\n", len);
             uint16_t *out = malloc(jksn_utf8_to_utf16(NULL, buf, len) * sizeof (uint16_t));
             fwrite(out, sizeof (uint16_t), jksn_utf8_to_utf16(out, buf, len), stdout);
             free(out);
